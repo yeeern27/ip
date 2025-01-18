9bbee7860979813efdec040b83a12d02774467c0
yeeern27 <gye2710@gmail.com>
2025-01-18 15:27:52 +0800

    Chatbot Renamed, Greeted and Exit

---
+++ src/main/java/Ern.java
@@ -0,0 +1,19 @@
+public class Ern {
+    public static void main(String[] args) {
+        String logo = " ____             \n"
+                     + "|  __|  ___  ____ \n"
+                     + "| |__ / ___|  __ |\n"
+                     + "|  __|  |  | | | |  \n"
+                     + "| |__|  |  | | | |  \n"
+                     + "|____|__|  |_| |_|\n";
+        System.out.println("Hello! I'm \n" + logo);
+        System.out.println("What can I do for you?");
+        String line = "_______________________________\n";
+        System.out.println(line);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(line);
+    }
+
+
+
+}
