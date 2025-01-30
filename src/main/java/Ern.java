7958eb2dfaafd327b803a0fee4f7f13bd9610ded
yeeern27 <gye2710@gmail.com>
2025-01-30 15:16:07 +0800

    Chatbot Made to Echo Input

--- src/main/java/Ern.java
+++ src/main/java/Ern.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Ern {
     public static void main(String[] args) {
         String logo = " ____             \n"
@@ -10,8 +12,22 @@ public class Ern {
         System.out.println("What can I do for you?");
         String line = "_______________________________\n";
         System.out.println(line);
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(line);
+
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNextLine()) {
+            String input = scanner.nextLine();
+
+            if (input.equalsIgnoreCase("bye")) {
+                System.out.println(line);
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println(line);
+                break;
+            }
+            System.out.println(line);
+            System.out.println(input);
+            System.out.println(line);
+        }
+        scanner.close();
     }
 
 
