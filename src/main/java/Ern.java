4a8bf9fb166a4d1d0fd78192bb2b9ac40bf6fe62
yeeern27 <gye2710@gmail.com>
2025-01-31 13:39:15 +0800

    Level-3 : Marked as Done

--- src/main/java/Ern.java
+++ src/main/java/Ern.java
@@ -1,21 +1,22 @@
 import java.util.Scanner;
 import java.util.ArrayList;
 
+
 public class Ern {
     public static void main(String[] args) {
         String logo = " ____             \n"
-                     + "|  __|  ___  ____ \n"
-                     + "| |__ / ___|  __ |\n"
-                     + "|  __|  |  | | | |  \n"
-                     + "| |__|  |  | | | |  \n"
-                     + "|____|__|  |_| |_|\n";
+                + "|  __|  ___  ____ \n"
+                + "| |__ / ___|  __ |\n"
+                + "|  __|  |  | | | |  \n"
+                + "| |__|  |  | | | |  \n"
+                + "|____|__|  |_| |_|\n";
         System.out.println("Hello! I'm \n" + logo);
         System.out.println("What can I do for you?");
         String line = "_______________________________\n";
         System.out.println(line);
 
         Scanner scanner = new Scanner(System.in);
-        ArrayList<String> tasks = new ArrayList<>();
+        ArrayList<Task> tasks = new ArrayList<>();
         while (scanner.hasNextLine()) {
             String input = scanner.nextLine();
 
@@ -26,17 +27,42 @@ public class Ern {
                 break;
             } else if (input.equalsIgnoreCase("list")) {
                 System.out.println(line);
+                System.out.println("Here are the tasks in your list: ");
                 for (int i = 0; i < tasks.size(); i++) {
-                    System.out.println((i + 1) + ". " + tasks.get(i));
+                    Task task = tasks.get(i);
+                    System.out.println(" " + (i + 1) + ". [" + task.getStatusIcon() + "]" + task.getDescription());
                 }
                 System.out.println(line);
-            } else {
-                    tasks.add(input);
+            } else if (input.toLowerCase().startsWith("mark ")) {
+                int index = Integer.parseInt(input.substring(5).trim()) - 1;
+                if (index >= 0 && index < tasks.size()) {
+                    tasks.get(index).markAsDone();
                     System.out.println(line);
-                    System.out.println("Added: " + input);
+                    System.out.println("Nice! I have marked this as Done!: ");
+                    System.out.println("[" + tasks.get(index).getStatusIcon() + "] " + tasks.get(index).getDescription());
                     System.out.println(line);
+                } else {
+                    System.out.println("Invalid index!");
                 }
+            } else if (input.toLowerCase().startsWith("unmark ")) {
+                int index = Integer.parseInt(input.substring(7).trim()) - 1;
+                if (index >= 0 && index < tasks.size()) {
+                    tasks.get(index).markAsNotDone();
+                    System.out.println(line);
+                    System.out.println("OK! I have marked this as Not Done!: ");
+                    System.out.println("[" + tasks.get(index).getStatusIcon() + "] " + tasks.get(index).getDescription());
+                    System.out.println(line);
+                } else {
+                    System.out.println("Invalid index!");
+                }
+            } else {
+                tasks.add(new Task(input));
+                System.out.println(line);
+                System.out.println("Added: " + input);
+                System.out.println(line);
             }
+        }
         scanner.close();
     }
 }
+
