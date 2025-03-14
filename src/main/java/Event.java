c7f039effe9f773232cb359da3f7e3be15cf1214
yeeern27 <gye2710@gmail.com>
2025-03-14 15:33:39 +0800

    Level 9 - Code Edited Find Tasks using keywords

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,14 +1,18 @@
-public class Event extends Task {
-    protected String from;
-    protected String to;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
-    public Event(String description, String from, String to) {
+public class Event extends Task {
+    protected LocalDateTime from;
+    protected LocalDateTime to;
+
+    public Event(String description, LocalDateTime from, LocalDateTime to) throws MyException {
         super(description);
         this.from = from;
         this.to = to;
     }
 
     public String toString() {
-        return "E" + " | " + (isDone ? "done" : "not done" ) + " | " + description + " | " + from + " | " + to;
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMM dd yyyy, HH:mm");
+        return "E" + " | " + (isDone ? "done" : "not done" ) + " | " + description + " | " + formatter.format(from) + " | " + formatter.format(to);
     }
 }
